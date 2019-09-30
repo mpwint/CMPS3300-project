30bbff1f0b0bcbf3ba88e0290eaebec82015d926
Ubuntu <ubuntu@ip-172-31-15-41.us-east-2.compute.internal>
2019-09-30 00:36:14 +0000

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+some_users = [
+  {:name => 'Mary', :email => 'mpwint@tulane.edu', :major => 'CS'},
+  {:name => 'Zach', :email => 'zseymour@tulane.edu', :major => 'CS'}
+]
+  
+some_users.each do |user|
+  User.create!(user)
+end
