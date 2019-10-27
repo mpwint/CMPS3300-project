d2b77c3e0b95422ebfef3bb5f76d70aa6adfbe9f
mpwint <mpwint@tulane.edu>
2019-10-27 20:33:23 +0000

    Add relation between two tables, user_id to post, and link 'show' action to post description

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_004959) do
+ActiveRecord::Schema.define(version: 2019_10_27_201723) do
 
   create_table "posts", force: :cascade do |t|
     t.string "poster_email"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
