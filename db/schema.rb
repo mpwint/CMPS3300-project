a16f1bae6b09615fddb1fe2f513f34b783028e6b
mpwint <mpwint@tulane.edu>
2019-11-03 13:15:48 +0000

    Add devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_27_201348) do
+ActiveRecord::Schema.define(version: 2019_11_03_130537) do
 
   create_table "posts", force: :cascade do |t|
     t.string "poster_email"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 2019_10_27_201348) do
     t.string "major"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
