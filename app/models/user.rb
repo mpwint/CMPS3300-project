edaf4f78e310bb5ee4d2a1166c3abf3ca4179f06
mpwint <mpwint@tulane.edu>
2019-11-03 14:02:19 +0000

    Add user authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :email, presence: true
+  
+  # Setup accessible (or protected) attributes for the model
+  # attr_accessible :password, :password_confirmation
 end
