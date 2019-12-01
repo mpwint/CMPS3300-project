ee9e1725d0e71eb9993230d60073bf92ade6e7c8
mpwint <mpwint@tulane.edu>
2019-12-01 05:51:37 +0000

    Fix name input validation bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-  # validates :name, presence: true
+  validates :name, presence: true
   validates :email, presence: true
   
   # Setup accessible (or protected) attributes for the model
