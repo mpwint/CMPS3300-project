3c847ff93b4d9f1ac7e734c62c3f6e00cb09dbc2
ZacharySeymour <ubuntu@ip-172-31-33-218.ec2.internal>
2019-12-03 01:56:49 +0000

    fixed routing error with Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   require 'commontator'
   mount Commontator::Engine => '/commontator'
+  devise_for controllers: {
+          :sessions => "users/sessions",
+          :registrations => "users/registrations" }
   devise_for :users
-  devise_for :users, controllers: {
-           :sessions => "users/sessions",
-           :registrations => "users/registrations" }
   resources :posts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
