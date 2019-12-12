9cd62e95840c8949f541be19f84dabc771541204
Ubuntu <ubuntu@ip-172-31-18-181.us-east-2.compute.internal>
2019-12-12 00:24:44 +0000

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   require 'commontator'
   mount Commontator::Engine => '/commontator'
-  devise_for :users, controllers: {
+  devise_for :users
+  devise_for controllers: {
            :sessions => "users/sessions",
-           :registrations => "users/registrations" }
+           :registrations => "users/registrations"
+    
+  }
   resources :posts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
